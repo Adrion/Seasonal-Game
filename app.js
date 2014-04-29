914a20ab01d0952217477b23afcfb211860c4694
Cedric Frossard ( Adrion ) <frossardcedric@gmail.com>
2014-04-29 16:56:00 +0200

    add live scores

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ var http = require('http'),
 
 var app = express(),
   server = http.createServer(app);
-console.log(app);
 
 //configure APP
 app.use("/www", express.static(__dirname + '/www'));
@@ -29,7 +28,7 @@ app.get('/', function (req, res) {
 // Ces variables resteront durant toute la vie du seveur et sont communes pour chaque client (node server.js)
 // Liste des messages de la forme { pseudo : 'Mon pseudo', message : 'Mon message' }
 var messages = [],
-  users = [],
+  users = {},
   scores = {};
 
 //// SOCKET.IO ////
@@ -41,28 +40,37 @@ io = io.listen(server);
 
 // Quand une personne se connecte au serveur
 io.sockets.on('connection', function (socket) {
-  console.log(socket);
+  var me;
   // On enregistre le nouveau joueur dans la partie.
   socket.on('register', function (pseudo) {
 
+    console.log(users);
     //On envoit la liste des joueurs connectés
-    socket.emit('getUsers', users);
+    //socket.emit('getUsers', users);
+    socket.emit("getUsers", users);
 
     //On enregistre le nouveau joueur dans la liste.
-    users[pseudo] = 0;
+    users[pseudo] = {
+      name: pseudo,
+      score: 0
+    };
+    me = pseudo;
 
     // On previent sa connexion à l'adversaire.
-    socket.broadcast.emit('userConnected', pseudo);
+    socket.broadcast.emit('userConnected', users[pseudo]);
   });
 
   // Quand on reçoit une action d'un joueur
-  socket.on('getPoint', function (dataPlayer) {
-
+  socket.on('getPoint', function (score) {
+    console.log('======>>' + score);
     // On modifie le score joueur (variable globale commune à tous les clients connectés au serveur)
-    users[dataPlayer.user] = dataPlayer.score;
-
+    users[me] = {
+      name: me,
+      score: score
+    };
+    console.log(users[me]);
     // On envoie à tout les clients connectés (sauf celui qui a appelé l'événement) les nouveaux scores
-    socket.broadcast.emit('refreshScore', users);
+    socket.broadcast.emit('refreshScore', users[me]);
   });
 });
 
